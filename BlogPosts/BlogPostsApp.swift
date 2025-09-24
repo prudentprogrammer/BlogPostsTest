bb6274e662cdb553e5d6ee55d9cc677a2567d097
Arjun Bharadwaj <arjunbharadwaj94@gmail.com>
2025-09-24 22:09:05 +0530

    Initial Commit

---
+++ BlogPosts/BlogPostsApp.swift
@@ -0,0 +1,17 @@
+//
+//  BlogPostsApp.swift
+//  BlogPosts
+//
+//  Created by Lakshmi Bharadwaj on 9/24/25.
+//
+
+import SwiftUI
+
+@main
+struct BlogPostsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
